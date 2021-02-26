aa107f3611ef0ff556e5a5c4faf6f9d3d9382ef5
praveen111 <praveen11_singh@yahoo.co.in>
2021-02-26 08:42:40 +0000

    First commit

---
+++ BlobStorageDemo/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using System;
+
+namespace BlobStorageDemo
+{
+    public static class ConnectionString
+    {
+        static string account = CloudConfigurationManager.GetSetting("arena11storage");
+        static string key = CloudConfigurationManager.GetSetting("jV1YbiOkX06nmDyNkyJvl5raNMeGa+URoQc+g7BaY5CAMv4gRWer5hwhgvjrI/8VhuSgCtZvRiTekjqFZWxv1Q==");
+
+        public static CloudStorageAccount GetConnectionString()
+        {
+            string connectionString = string.Format(@"DefaultEndpointsProtocol=https;AccountName=arena11storage;AccountKey=jV1YbiOkX06nmDyNkyJvl5raNMeGa+URoQc+g7BaY5CAMv4gRWer5hwhgvjrI/8VhuSgCtZvRiTekjqFZWxv1Q==;EndpointSuffix=core.windows.net");
+            return CloudStorageAccount.Parse(connectionString);
+        }
+    }
+}
